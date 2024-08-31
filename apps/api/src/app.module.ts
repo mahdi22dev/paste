a3c0d05346fafdef7ebb160c903b68004bb86c80
mahdi22dev <idrissimahdi2020@gmail.com>
2024-08-31 04:01:00 +0100

    d

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,10 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
-import { LoggerMiddleware } from './midddleware/logger.middleware';
 
 @Module({
   imports: [
@@ -18,8 +17,4 @@ import { LoggerMiddleware } from './midddleware/logger.middleware';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('users');
-  }
-}
+export class AppModule {}
